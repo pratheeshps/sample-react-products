cd16b1ea6fe1274c1cdc50fe0fc4c6c210daf490
Pratheesh P S <pratheeshps.me@gmail.com>
2023-03-13 11:27:30 +0000

    Update 10 files

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -6,12 +6,23 @@ function Product() {
   const { id } = useParams();
   const { data, loading } = useFetch(`${GET_PRODUCTS_URL}/${id}`);
 
+  if (loading) {
+    return <p>Loading....</p>;
+  }
+
   return (
     <>
       {data && (
         <div>
           <h1>{data.title}</h1>
+          <b>Rating : {data.rating}</b>
+          <div className="product-images">
+            {data.images.map((img) => (
+              <img src={img} width="100" height="100" />
+            ))}
+          </div>
           <p>{data.description}</p>
+          <h3>${data.price}</h3>
         </div>
       )}
     </>
